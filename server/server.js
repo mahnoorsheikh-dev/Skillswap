1f9aa63db27bf2a0a4fe685f9adcd07e6b451b01
mahnoorsheikh-dev <mahnoor.3896@gmail.com>
2026-06-08 09:39:08 +0500

    Initial Project setup

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend Running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
